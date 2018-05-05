fcf095adeb116860ec5771974b6f0e013bd60a86
Jason Reinhardt <jason.w.reinhardt@gmail.com>
2018-05-05 22:59:20 +0200

    180505_02
    Imported old assets - wow are things fucked up

---
+++ Assets/Scripts/Eye.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eye  {
+
+    public Matrix4x4 Extrinsics;
+    public Matrix4x4 Intrinsics;
+    public Vector2 UV;
+
+}
---
+++ Assets/Scripts/EyeCollection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EyeCollection : MonoBehaviour {
+
+    public List<Eye> eyes;
+
+    public List<Eye> GetEyes()
+    {
+        return eyes;
+    }
+
+
+
+}
